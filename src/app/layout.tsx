63f76ecfde8e84fa2816ab204e71e71bd9f82475
Paulo Dantas <paulodantasbjr@gmail.com>
2024-03-10 16:29:34 +0000

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,10 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
+
 import { ThemeProvider } from "@/components/theme-provider";
+import "@/styles/globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
